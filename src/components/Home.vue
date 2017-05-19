23b134c8c36c37443e7e7481922baae92acf3aa4
Wenish <jonas.voland@bluewin.ch>
2017-05-19 22:49:43 +0200

    -

---
+++ src/components/Home.vue
@@ -0,0 +1,127 @@
+<template>
+<div class="home">
+  <h1>{{ msg }}</h1>
+  <h2>Essential Links</h2>
+  <ul>
+    <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+    <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+    <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+    <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <br>
+    <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+  </ul>
+  <h2>Ecosystem</h2>
+  <ul>
+    <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+    <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+    <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+    <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+  </ul>
+  <div class="panel-body">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in books">
+          <td><a v-bind:href="book.url" target="_blank">{{book.title}}</a></td>
+          <td>{{book.author}}</td>
+          <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeBook(book)">X</span></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Add New Books</h3>
+    </div>
+    <div class="panel-body">
+      <form id="form" class="form-inline" @submit.prevent="addBook">
+        <div class="form-group">
+          <label for="bookTitle">Title:</label>
+          <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+        </div>
+        <div class="form-group">
+          <label for="bookAuthor">Author:</label>
+          <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+        </div>
+        <div class="form-group">
+          <label for="bookUrl">Url:</label>
+          <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Add Book">
+      </form>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyDFHGGSjrSzeyPEfGIMGg6JGDY0EV8ZbGI',
+  authDomain: 'pure-83e27.firebaseapp.com',
+  databaseURL: 'https://pure-83e27.firebaseio.com',
+  projectId: 'pure-83e27',
+  storageBucket: 'pure-83e27.appspot.com',
+  messagingSenderId: '476647766797'
+}
+
+const app = Firebase.initializeApp(config)
+const db = app.database()
+const booksRef = db.ref('books')
+
+export default {
+  name: 'home',
+  firebase: {
+    books: booksRef
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      newBook: {
+        title: '',
+        author: '',
+        url: 'http://'
+      }
+    }
+  },
+  methods: {
+    addBook: function () {
+      booksRef.push(this.newBook)
+      this.newBook.title = ''
+      this.newBook.author = ''
+      this.newBook.url = 'http://'
+    },
+    removeBook: function (book) {
+      booksRef.child(book['.key']).remove()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Text-Field.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-field">
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+const value = value || ''
+export default {
+  name: 'text-field',
+  data () {
+    return {
+      value: value
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  background: red;
+}
+</style>
